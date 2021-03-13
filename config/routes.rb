9b65133a976810f0cc598118672d69dd4b3fced1
Colin Alexander <colintalex@gmail.com>
2021-03-13 05:13:06 -0700

    Adds road trip route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       get '/forecast', to: 'forecasts#show'
       get '/backgrounds', to: 'backgrounds#show'
       post '/users', to: 'users#create'
+      post '/road_trip', to: 'road_trips#create'
     end
   end
 end
