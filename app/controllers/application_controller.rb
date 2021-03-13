d4be3f88afd93c1582202d6cc04b806147fd8704
Colin Alexander <colintalex@gmail.com>
2021-03-13 05:12:34 -0700

    Adds API key auth to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+
+    private
+
+    def authenticate_api_key
+        user = User.find_by(api_key: trip_params['api_key'])
+        user.nil? ? bad_api_key : return
+    end
+
+    def bad_api_key
+        bad_key_error = Error.new('Unauthorized access. Sign up for API key, or check your current one')
+        render json: ErrorSerializer.new(bad_key_error), status: 400
+    end
 end
