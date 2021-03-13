0e5c093897d7b5e1698a6b9613190ec0cbcacb55
Colin Alexander <colintalex@gmail.com>
2021-03-13 03:35:30 -0700

    Checks for empty location params

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -1,9 +1,14 @@
 class Api::V1::ForecastsController < ApplicationController
 
     def show
-        coords = get_location_coordinates(forecast_params[:location])
-        weather = get_weather_by_coordinates(coords)
-        render json: CityWeatherSerializer.new(weather)
+        #check for valid coords
+        if forecast_params[:location] == ''
+            render json: Error.new("Location can't be empty") 
+        else
+            coords = get_location_coordinates(forecast_params[:location])
+            weather = get_weather_by_coordinates(coords)
+            render json: CityWeatherSerializer.new(weather)
+        end
     end
 
     private
@@ -13,7 +18,7 @@ class Api::V1::ForecastsController < ApplicationController
     end
     
     def get_location_coordinates(location)
-        MapQuestService.new.get_coordinates(location)
+        coords = MapQuestService.new.get_coordinates(location)
     end
 
     def get_weather_by_coordinates(coords)
