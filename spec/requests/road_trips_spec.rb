a3f782024b6ab56d9233104e0b550a751694c84b
Colin Alexander <colintalex@gmail.com>
2021-03-14 03:19:12 -0600

    Adds green path spec test for rodtrip endpoint, 100% coverage

--- spec/requests/road_trips_spec.rb
+++ spec/requests/road_trips_spec.rb
@@ -2,20 +2,29 @@ require 'rails_helper'
 
 RSpec.describe "RoadTrips", type: :request do
   describe "GET /road_trips" do
+    before(:each) do 
+      @user = User.create({
+        email: 'c@g.com',
+        password: 'password',
+        password_confirmation: 'password'
+      })
+    end
     it "works! (now write some real specs)" do
+      
       body = {
         "origin": "Denver,CO",
         "destination": "Pueblo,CO",
-        "api_key": "RUNJjbtr8nfe5RYTnA9ApeoV"
+        "api_key": @user.api_key
       }
 
       post '/api/v1/road_trip', params: { road_trip: body }
       
-      expect(response).to have_http_status(200)
       body = JSON.parse(response.body, symbolize_names: true)
 
+      expect(response).to have_http_status(200)
+
       expect(body[:data]).to have_key(:id)
-      expect(body[:data][:id]).to eql('null')
+      expect(body[:data][:id]).to eql(nil)
       expect(body[:data]).to have_key(:type)
       expect(body[:data][:type]).to eql('roadtrip')
       expect(body[:data]).to have_key(:attributes)
@@ -24,8 +33,21 @@ RSpec.describe "RoadTrips", type: :request do
       expect(body[:data][:attributes]).to have_key(:travel_time)
       expect(body[:data][:attributes]).to have_key(:weather_at_eta)
       expect(body[:data][:attributes][:weather_at_eta]).to have_key(:temperature)
-      expect(body[:data][:attributes][:weather_at_eta][:temperature]).to type_of(float)
       expect(body[:data][:attributes][:weather_at_eta]).to have_key(:conditions)
+    end
+
+    it "doesn't work with bad api key" do
+      body = {
+        "origin": "Denver,CO",
+        "destination": "Pueblo,CO",
+        "api_key": "supahkey"
+      }
+
+      post '/api/v1/road_trip', params: { road_trip: body }
+      
+      body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to have_http_status(400)
     end
   end
 end
