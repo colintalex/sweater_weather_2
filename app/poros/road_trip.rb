ae9787c26c8965029213b378564164ece8c4d35c
Colin Alexander <colintalex@gmail.com>
2021-03-14 03:20:28 -0600

    Adds attributes and init for RoadTrip poro

--- app/poros/road_trip.rb
+++ app/poros/road_trip.rb
@@ -1,3 +1,9 @@
 class RoadTrip
-    
+    attr_reader :id, :start_city, :end_city, :travel_time, :weather_at_eta
+    def initialize(travel_data, eta_weather_data, trip_params)
+        @start_city = trip_params[:origin]
+        @end_city = trip_params[:destination]
+        @travel_time = travel_data[:eta]
+        @weather_at_eta = eta_weather_data
+    end
 end
